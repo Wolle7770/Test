fb8efcdb321a888cc5978ef63f5c68d2c052a753
Wollkopf03 <69468067+Wollkopf03@users.noreply.github.com>
2022-04-14 01:43:07 +0200

    Update HelloWorld.java

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -3,10 +3,10 @@ package main;
 public class HelloWorld {
     public static void main(String[] args) {
         new HelloWorld();
+        System.exit(0);
     }
 
     public HelloWorld(){
         System.out.println("Hello World!");
-        System.exit(0);
     }
 }
